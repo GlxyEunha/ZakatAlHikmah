e7f1f5d0aaccb817709522f93ff01c46dddcf1ba
GlxyEunha <ivandapratama2@gmail.com>
2025-03-14 20:54:35 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,20 @@ Route::middleware('auth.custom')->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::get('/rekap-zakat', function () {
-        return view('zakat');
-    })->name('rekap_zakat');
-    Route::get('/rekap-pemohon', function () {
-        return view('pemohon');
-    })->name('rekap_pemohon');
-    Route::get('/rekap-pengeluaran', function () {
-        return view('pengeluaran');
-    })->name('rekap_pengeluaran');
-    Route::get('/zakat-form', [ZakatController::class, 'index_zakat'])->name('form-zakat');
+    Route::get('/rekap-zakat', [ZakatController::class, 'index_zakat'])->name('rekap_zakat');
+    Route::get('/rekap-pemohon', [ZakatController::class, 'index_pemohon'])->name('rekap_pemohon');
+    Route::get('/rekap-pengeluaran', [ZakatController::class, 'index_pengeluaran'])->name('rekap_pengeluaran');
+
+    Route::get('/zakat-form', function () {
+        return view('form_zakat');
+    })->name('form-zakat');
     Route::post('/zakat', [ZakatController::class, 'store_zakat'])->name('zakat');
-    Route::get('/pemohon-form', [ZakatController::class, 'index_pemohon'])->name('form-pemohon');
+    Route::get('/pemohon-form', function () {
+        return view('form_pemohon');
+    })->name('form-pemohon');
     Route::post('/pemohon', [ZakatController::class, 'store_pemohon'])->name('pemohon');
-    Route::get('/pengeluaran-form', [ZakatController::class, 'index_pengeluaran'])->name('form-pengeluaran');
+    Route::get('/pengeluaran-form', function () {
+        return view('form_pengeluaran');
+    })->name('form-pengeluaran');
     Route::post('/pengeluaran', [ZakatController::class, 'store_pengeluaran'])->name('pengeluaran');
 });
